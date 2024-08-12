63d8e421595dfbbecfc9d86ef80ebb57a1977dee
Abin <abin>
2024-08-12 20:10:52 +0530

    Utils added

---
+++ src/utils/staticImgs.jsx
@@ -0,0 +1,14 @@
+// Import images
+import one from '../assets/numbers/1.png';
+import two from '../assets/numbers/2.png';
+import three from '../assets/numbers/3.png';
+import four from '../assets/numbers/4.png';
+import five from '../assets/numbers/5.png';
+import six from '../assets/numbers/6.png';
+import seven from '../assets/numbers/7.png';
+import eight from '../assets/numbers/8.png';
+import nine from '../assets/numbers/9.png';
+import ten from '../assets/numbers/10.png';
+
+// Export images
+export { one, two, three, four, five, six, seven, eight, nine, ten };
