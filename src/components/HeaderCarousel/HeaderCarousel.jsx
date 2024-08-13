cef3caeac3b2c42b45149daa25f8efa584f41a79
Abin <abin>
2024-08-13 12:39:47 +0530

    Icons added

--- src/components/HeaderCarousel/HeaderCarousel.jsx
+++ src/components/HeaderCarousel/HeaderCarousel.jsx
@@ -1,13 +1,16 @@
 import { useState, useRef, useEffect } from "react";
 import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import { one, two, three, four, five, six, seven, eight } from '../../utils/CarouselImg';
+import { FaPlay } from "react-icons/fa";
+import { IoVideocamSharp } from "react-icons/io5";
+
 
 function HeroCarousel() {
     const staticImages = [one, two, three, four, five, six, seven, eight];
 
     const [mouseHover, setMouseHover] = useState(false);
     const carouselRef = useRef(null);
-    
+
     // Automatically scroll every 5 seconds
     useEffect(() => {
         const interval = setInterval(() => {
@@ -45,8 +48,13 @@ function HeroCarousel() {
             className="relative flex items-center justify-center w-full px-4 lg:px-0 mb-20"
         >
             <button
-                className={`${mouseHover ? 'block' : 'hidden'
-                    } flex justify-center items-center absolute left-2 md:left-4 lg:left-5 top-1/2 transform -translate-y-1/2 pl-2 bg-gray-900 text-white h-8 w-8 md:h-10 md:w-10 rounded-full`}
+                className={`${mouseHover ? 'block' : 'hidden'} 
+        flex justify-center items-center 
+        absolute left-2 md:left-4 lg:left-5 
+        top-1/2 transform -translate-y-1/2 
+        p-2 bg-gray-900 text-white 
+        h-8 w-8 md:h-10 md:w-10 rounded-full
+        z-10`}
                 onClick={handlePrevClick}
             >
                 <MdArrowBackIos className="h-4 w-4 md:h-5 md:w-5" />
@@ -56,15 +64,30 @@ function HeroCarousel() {
                 className="carousel carousel-center w-full rounded-box space-x-4 overflow-hidden"
             >
                 {staticImages.map((image, index) => (
-                    <div key={index} className="carousel-item w-[100vw] md:w-[60vw] lg:w-[62.5rem] h-[30vw] md:h-[10rem] lg:h-[23.438rem] flex-shrink-0">
+                    <div
+                        key={index}
+                        className="carousel-item w-[100vw] md:w-[60vw] lg:w-[62.5rem] h-[30vw] md:h-[10rem] lg:h-[23.438rem] flex-shrink-0 relative group"
+                    >
+                        <div className="absolute inset-0 rounded-box bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300"></div>
                         <img
                             src={image}
-                            className="rounded-box w-full h-full object-cover hover:border-[#d1ff00] border-4 border-transparent"
+                            className="rounded-box w-full h-full object-cover border-4 border-transparent group-hover:border-[#d1ff00] transition-border duration-300"
                             alt={`Carousel item ${index + 1}`}
                         />
+                        <div className="absolute bottom-10 right-10 flex items-center gap-5 space-x-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <div>
+                                <FaPlay className="text-[#d1ff00] text-5xl cursor-pointer" />
+                                <h1>Play</h1>
+                            </div>
+                            <div>
+                                <IoVideocamSharp className="text-[#d1ff00] text-6xl cursor-pointer" />
+                                <h1>Triler</h1>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
+
             <button
                 className={`${mouseHover ? 'block' : 'hidden'
                     } flex justify-center items-center absolute right-2 md:right-4 lg:right-5 top-1/2 transform -translate-y-1/2 p-2 bg-gray-900 text-white h-8 w-8 md:h-10 md:w-10 rounded-full`}
